c8b7f37e2467e9bfa270d8da35e1cca59f56da7e
skyJinc <jincan0213@hotmail.com>
2015-01-31 21:56:59 +0800

    修改构建文件

--- app/src/androidTest/java/j2w/team/ApplicationTest.java
+++ app/src/androidTest/java/j2w/team/ApplicationTest.java
@@ -1,16 +1,11 @@
 package j2w.team;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
 
 import j2w.team.common.log.L;
 
-@RunWith(RobolectricTestRunner.class) public class ApplicationTest {
+ public class ApplicationTest {
 
-	@Test public void print_log() throws Exception {
-        L.init(true);
-
+	public void print_log() throws Exception {
         L.i("打印思密达");
     }
 }
