e8f49aa46fbe0b87f8b692ef4b8560579ebca45d
skyJinc <jincan0213@hotmail.com>
2015-03-01 00:53:42 +0800

    增加dialog

---
+++ app/src/main/java/j2w/team/modules/dialog/J2WDialogBuilder.java
@@ -0,0 +1,8 @@
+package j2w.team.modules.dialog;
+
+/**
+ * Created by sky on 15/3/1. Dialog编辑器
+ */
+public class J2WDialogBuilder<T extends J2WDialogBuilder<T>> {
+    
+}
---
+++ app/src/main/java/j2w/team/modules/dialog/J2WDialogFragment.java
@@ -0,0 +1,16 @@
+package j2w.team.modules.dialog;
+
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by sky on 15/2/28. dialog 基类
+ */
+public class J2WDialogFragment extends DialogFragment implements DialogInterface.OnShowListener {
+
+    
+    
+	@Override public void onShow(DialogInterface dialog) {
+
+	}
+}
