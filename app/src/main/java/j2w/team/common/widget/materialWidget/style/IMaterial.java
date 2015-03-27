ba3640b6df658ded72c9829aa52385eac19f4768
wangtao <alison_mahone@163.com>
2015-03-27 14:01:31 +0800

    添加控件/布局点击背景波纹效果
    把常用的组件、布局自定义了，有需要的地方可以单独引用使用。
    使用方法在项目wiki中。

---
+++ app/src/main/java/j2w/team/common/widget/materialWidget/style/IMaterial.java
@@ -0,0 +1,10 @@
+package j2w.team.common.widget.materialWidget.style;
+
+public interface IMaterial {
+	public void perfirmSuperClick();
+
+	public void setColor(int fullColor);
+
+	public void setType(int widgetType);
+
+}
