b772ae8cc812f43d68189fe09c2228f4e33bc291
skyJinc <jincan0213@hotmail.com>
2015-06-29 19:30:59 +0800

    修改

--- app/src/main/java/j2w/team/common/widget/dragtoplayout/AttachUtil.java
+++ app/src/main/java/j2w/team/common/widget/dragtoplayout/AttachUtil.java
@@ -6,44 +6,47 @@ import android.widget.AbsListView;
 import android.widget.ScrollView;
 
 /**
- * Created by chenupt@gmail.com on 1/30/15.
- * Description : Attach top helper
+ * Created by chenupt@gmail.com on 1/30/15. Description : Attach top helper
  */
 public class AttachUtil {
 
-    public static boolean isAdapterViewAttach(AbsListView listView){
-        if (listView != null && listView.getChildCount() > 0) {
-            if (listView.getChildAt(0).getTop() < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public static boolean isAdapterViewAttach(AbsListView listView) {
+		if (listView != null && listView.getChildCount() > 0) {
+			if(listView.getFirstVisiblePosition() != 0){
+				return false;
+			}
 
-    public static boolean isRecyclerViewAttach(RecyclerView recyclerView){
-        if (recyclerView != null && recyclerView.getChildCount() > 0) {
-            if (recyclerView.getChildAt(0).getTop() < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+			if (listView.getChildAt(0).getTop() < 0 ) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public static boolean isScrollViewAttach(ScrollView scrollView){
-        if (scrollView != null) {
-            if (scrollView.getScrollY() > 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public static boolean isRecyclerViewAttach(RecyclerView recyclerView) {
+		if (recyclerView != null && recyclerView.getChildCount() > 0) {
+			if (recyclerView.getChildAt(0).getTop() < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public static boolean isWebViewAttach(WebView webView){
-        if (webView != null) {
-            if (webView.getScrollY() > 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public static boolean isScrollViewAttach(ScrollView scrollView) {
+		if (scrollView != null) {
+			if (scrollView.getScrollY() > 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isWebViewAttach(WebView webView) {
+		if (webView != null) {
+			if (webView.getScrollY() > 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
